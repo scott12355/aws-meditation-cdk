13e36591263adc8746d7da0d041173acedbd7a41
scott12355 <scott.r.topping@gmail.com>
2025-05-14 21:20:16 +0100

    Initial commit

---
+++ test/aws-meditation-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsMeditationCdk from '../lib/aws-meditation-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-meditation-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsMeditationCdk.AwsMeditationCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
